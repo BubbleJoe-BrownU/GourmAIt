047906be031905aa6c1979754374b982bc12c05f
BubbleJoe-BrownU <jiayuzheng99@gmail.com>
2023-08-30 15:05:15 -0400

    start to scriptize training

--- train_noisy_student.py
+++ train_noisy_student.py
@@ -11,7 +11,46 @@ from torchvision.models.resnet import resnet18, resnet34, resnet50, ResNet18_Wei
 
 from randaug import RandAugment
 
-def prepare_models():
+# configs
+out_dir = 'out-resnet50-pretrained-stepwise-unfreeze'
+dataset_dir = 'datasets'
+
+rand_aug = True
+stepwise_unfreeze = True
+init_from = 'from_pretrained'
+
+
+
+# system
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
+ptdtype = {
+    'float32': torch.float32,
+    'bfloat16': torch.bfloat16,
+    'float16': torch.float16
+}[dtype]
+to_compile = True
+
+os.makedirs(out_dir, exist_ok=True)
+
+batch_size = 128
+torch.manual_seed(42)
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cuda.allow_tf32 = True # allow tf32 on cudnn
+
+grad_clip = 1.0
+# learning rate decay settings
+decay_lr = True
+warmup_iters = 5 # how many steps to warm up for
+lr_decay_iters = 50
+learning_rate = 1e-3
+min_lr = 1e-5
+
+max_epochs = 100
+epoch_num = 0
+best_val_loss = float('inf')
+
+def prepare_training():
     model1 = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
     model1.fc = nn.Linear(in_features=512, out_features=101, bias=True)
     model2 = resnet34(weights=ResNet34_Weights.IMAGENET1K_V2)
@@ -22,9 +61,25 @@ def prepare_models():
     model4.fc = nn.Linear(in_features=204, out_features=101, bias=True)
     
     models = [model1, model2, model3, model4]
-    batch_size_list = []
-    return 
-    
+    batch_size_list = [128, 128, 96, 64]
+    epoch_list = [100, 50, 50, 50]
+    return models, batch_size_list, epoch_list
+
+# preprocess the input
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+# random augmentation
+random_augmentation_preprocess = transforms.Compose(
+    [
+        RandAugment(),
+        preprocess
+    ]
+)
+
 def get_batch_with_pseudo_label(batch_size, teacher, device, label_model='soft'):
     data = Food101(root=dataset_dir, split='train')
     
@@ -55,7 +110,7 @@ def main():
     We use the student as the new teacher model and continue to step 2.
     """
     
-    models = prepare_models()
+    models, batch_size_list, epoch_list = prepare_training()
     
     for i, model in enumerate(models):
         if i == 0:
@@ -65,8 +120,10 @@ def main():
             checkpoint_path = 'out-'
             model.load_state_dict(torch.load(checkpoint_path, map_location=device)['model'])
         elif i > 0:
+            batch_size = batch_size_list[i]
+            max_epochs = epoch_list[i]
             dataloader = get_batch_with_pseudo_label(batch_size, models[i-1], device)
-            for i
+            
             
     
     
